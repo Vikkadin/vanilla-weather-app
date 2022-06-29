414e822207d6b019ffddcd8310202854ef2507da
Vikkadin <104149770+Vikkadin@users.noreply.github.com>
2022-06-29 19:43:30 +0300

    Adding Search Engine

--- src/index.js
+++ src/index.js
@@ -44,7 +44,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "8ff5d1a1376b46b7fe89092d7988204d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "8ff5d1a1376b46b7fe89092d7988204d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#input-form");
+  search(cityInputElement.value);
+}
+
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
