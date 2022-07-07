491e3d40c2c1b32efd33a151d9dfcef35154ef7d
Vikkadin <104149770+Vikkadin@users.noreply.github.com>
2022-07-07 17:27:22 +0300

    Removing Pressure, Adding Paddings

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ function formatDate() {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  return `As of ${hour}:${minutes} ${day} ${date} of ${month}`;
+  return `As of ${hour}:${minutes}</br> ${day} ${date} of ${month}`;
 }
 
 function formatForecastDate(timestamp) {
@@ -162,7 +162,6 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#weather-description");
-  let pressureElement = document.querySelector("#pressure");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
@@ -172,7 +171,6 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
-  pressureElement.innerHTML = response.data.main.pressure;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt + 1000);
